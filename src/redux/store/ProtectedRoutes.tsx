02e97d7273e124d43c41b0908a26160bed97313b
Abdulhamid <mobolaji2309@gmail.com>
2023-10-15 08:24:27 +0100

    add protectedroute

--- src/redux/store/ProtectedRoutes.tsx
+++ src/redux/store/ProtectedRoutes.tsx
@@ -1,34 +1,32 @@
-import React, {useEffect, useState} from 'react'
-import { useAppDispatch, useAppSelector } from '../hooks'
-import { refreshToken, userLogged, verify } from '@/services/auth'
-import { loginRedux } from '../reducers/auth.slice'
-const ProtectedRoutes = ({children }: {children : React.ReactNode}) => {
-   const [showLoading, setShowLoading] = useState(true)
-   const dispatch = useAppDispatch()
-   const {isAuthenticated, isLoading} = useAppSelector((state) => state.auth)
-   useEffect(()=> {
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { refreshToken, userLogged, verify } from "@/services/auth";
+import { loginRedux } from "../reducers/auth.slice";
+const ProtectedRoutes = ({ children }: { children: React.ReactNode }) => {
+  const [showLoading, setShowLoading] = useState(true);
+  const dispatch = useAppDispatch();
+  const { isAuthenticated, isLoading } = useAppSelector((state) => state.auth);
+  useEffect(() => {
     async function checkAuth() {
-        if (!isAuthenticated && localStorage.getItem("access") !== null) {
-          try {
-            await refreshToken();
-            await verify()
-           const user = await userLogged()
-           dispatch(loginRedux(user))
-          } catch (error: any) {
-            localStorage.removeItem("access");
-            localStorage.removeItem("refresh");
-      
-            console.log(error.response.data);
-          }
-        } else {
-  setShowLoading(false)
-        }
-      }
-      checkAuth();
-   }, [])
-  return (
-    <div>ProtectedRoutes</div>
-  )
-}
+      if (!isAuthenticated && localStorage.getItem("access") !== null) {
+        try {
+          await refreshToken();
+          await verify();
+          const user = await userLogged();
+          dispatch(loginRedux(user));
+        } catch (error: any) {
+          localStorage.removeItem("access");
+          localStorage.removeItem("refresh");
 
-export default ProtectedRoutes
+          console.log(error.response.data);
+        }
+      } else {
+        setShowLoading(false);
+      }
+    }
+    checkAuth();
+  }, [isAuthenticated, isLoading]);
+  return <div>ProtectedRoutes</div>;
+};
+
+export default ProtectedRoutes;
