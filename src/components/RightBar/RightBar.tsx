b80b7ae86f2d02151a4ddbdfc63c5a2397347a44
Ham12-3 <mobolaji2309@gmail.com>
2023-10-22 18:08:42 +0100

    update

--- src/components/RightBar/RightBar.tsx
+++ src/components/RightBar/RightBar.tsx
@@ -1,40 +1,45 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export const RightBar = () => {
   return (
-   <div className='basis-[25%] sticky top-[85px] h-[calc(100vh-70px)]'>
-    <div>
-      <h4 className='text-lg font-bold text-gray-800 dark:text-white/70'>
-Sponsored
-      </h4>
-      <div className='px-3'>
-<div className='flex gap-x-4'>
-<div className='w-[45px] max-w-[50px] h-[45px] relative aspect-square' >
-  <Image 
-  src={'/pepsi-logo.png'}
-  alt='#'
-  fill
-  loading='lazy'
-  sizes="(max-width:45px) 100vw, 40px"
-  className='rounded-full cursor-pointer mt-3'
-  />
-</div>
-</div>
-<div className='relative w-fit max-w-[270px] h-[150px] mt-4 mx-auto aspect-video'>
-<Image 
-src={'/ive-pepsi.jpg'}
-alt='#'
-fill
-loading='lazy'
-sizes ='(max-width: 250px) 100vw, 200px, 150px'
-/>
-</div>
+    <div className="basis-[25%] sticky top-[85px] h-[calc(100vh-70px)]">
+      <div>
+        <h4 className="text-lg font-bold text-gray-800 dark:text-white/70">
+          Sponsored
+        </h4>
+        <div className="px-3">
+          <div className="flex gap-x-4">
+            <div className="w-[45px] max-w-[50px] h-[45px] relative aspect-square">
+              <Image
+                src={"/pepsi-logo.png"}
+                alt="#"
+                fill
+                loading="lazy"
+                sizes="(max-width:45px) 100vw, 40px"
+                className="rounded-full cursor-pointer mt-3"
+              />
+            </div>
+            <div className="text-[16px] font-semibold text-gray-800 dark:text-white/70">
+              <h4>Ive x Pepsi</h4>
+              <p className="text-gray-600 dark:text-white/50 text-xs"> Pepsi and I've team up for a new, one-of-a-kind flavor! try the new Pepsi Ive and enjoy the refeshing combination of Pepsi's taste and Ive's music. Available now!</p>
+            </div>
+          </div>
+          <div className="relative w-fit max-w-[270px] h-[150px] mt-4 mx-auto aspect-video">
+            <Image
+              src={"/ive-pepsi.jpg"}
+              alt="#"
+              fill
+              loading="lazy"
+              sizes="(max-width: 250px) 100vw, 200px, 150px"
+              className="rounded-md"
+            />
+          </div>
+        </div>
       </div>
+      <hr className="border-0 h-[1px] my-[15px] bg-gray-200/50 max-w-[80%] block mx-auto" />
     </div>
-    <hr  className='border-0 h-[1px] my-[15px] bg-gray-200/50 max-w-[80%] block mx-auto'/>
-   </div>
-  )
-}
+  );
+};
