ff9be16da097bdcff135cab0be8d5145428c2675
Ham12-3 <mobolaji2309@gmail.com>
2023-10-30 17:30:31 +0000

    update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "ivebook-backend.onrender.com/",
+        hostname: "ivebook-backend.onrender.com",
         port: "",
         pathname: "/**",
       },
