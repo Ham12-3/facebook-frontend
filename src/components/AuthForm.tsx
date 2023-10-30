c8c526919de3b8cfe1e20ea43df85817cf5ae34b
Ham12-3 <mobolaji2309@gmail.com>
2023-10-30 17:44:46 +0000

    update

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import { Form } from "@/interface/interface";
-import { BsFillImageFill } from "react-icons/bs";
 import Image from "next/image";
-
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import { BsFillImageFill } from "react-icons/bs";
 import Link from "next/link";
 import { login, register } from "@/services/auth";
 import toast, { Toaster } from "react-hot-toast";
@@ -16,7 +15,7 @@ export const AuthForm = ({ process }: { process: string }) => {
   const [prevImage, setPrevImage] = useState();
   const imgRef = useRef<HTMLInputElement>(null);
 
-const {dispatch, pathname, router, authChecked}=useAuth()
+  const { dispatch, pathname, router, authChecked } = useAuth();
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -24,14 +23,25 @@ const {dispatch, pathname, router, authChecked}=useAuth()
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
+  const fileSelected = (e: ChangeEvent<HTMLInputElement>) => {
+    setImage(e.target.files![0]);
+
+    const reader: any = new FileReader();
+    reader.readAsDataURL(e.target.files![0]);
+
+    reader.onloadend = () => {
+      setPrevImage(reader.result);
+    };
+  };
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (pathname === "/login") {
       try {
-        const {message, user} = await login(form);
+        const { message, user } = await login(form);
         toast.success(message, { duration: 1000 });
-        console.log(user)
         dispatch(loginRedux(user));
+
         setTimeout(() => {
           toast.dismiss();
           router.push("/");
@@ -67,132 +77,125 @@ const {dispatch, pathname, router, authChecked}=useAuth()
       }
     }
   };
-  const fileSelected = (e: ChangeEvent<HTMLInputElement>) => {
-    setImage(e.target.files![0]);
-    const reader: any = new FileReader();
-    reader.readAsDataURL(e.target.files![0]);
-    reader.onloadend = () => {
-      setPrevImage(reader.result);
-    };
-  };
   return (
     <>
-   {
-    authChecked ? (
-      <div
-      className="h-screen w-screen flex items-center justify-center"
-      style={{ background: "url(https://i.stack.imgur.com/vzbuQ.jpg)" }}
-    >
-      <form
-        onSubmit={handleSubmit}
-        className="relative flex-col flex w-[580px] justify-center gap-y-5 bg-white/[.03] py-10 px-12 backdrop-blur-[3px]"
-        action=""
-      >
-        <h1 className="text-5xl font-bold text-white"> {process} </h1>
-        <input
-          type="text"
-          placeholder="Username"
-          name="username"
-          className="input"
-          required
-          autoFocus
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          name="password"
-          className="input"
-          required
-          onChange={handleChange}
-        />
-        {pathname === "/register" && (
-          <>
+      {authChecked ? (
+        <div
+          className="h-screen w-screen flex items-center justify-center"
+          style={{ background: "url(https://i.stack.imgur.com/vzbuQ.jpg)" }}
+        >
+          <form
+            className="relative flex flex-col w-[580px] justify-center gap-y-5 bg-white/[.03] py-10 px-12 backdrop-blur-[3px]"
+            onSubmit={handleSubmit}
+          >
+            <h1 className="text-5xl font-bold text-white">{process}</h1>
             <input
-              type="email"
-              placeholder="Email"
-              name="email"
+              type="text"
+              placeholder="Username"
+              name="username"
+              className="input"
+              required
+              autoFocus
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
               className="input"
               required
               onChange={handleChange}
             />
-            <input
-              type="text"
-              placeholder="First Name"
-              name="first_name"
-              className="input"
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              placeholder="Last Name"
-              name="last_name"
-              className="input"
-              onChange={handleChange}
-            />
-            <textarea
-              placeholder="Bio ..."
-              name="bio"
-              rows={5}
-              className="input resize-none"
-              onChange={handleChange}
-            />
-            <input
-              className="hidden"
-              type="file"
-              name="image"
-              required
-              onChange={fileSelected}
-              ref={imgRef}
-            />
-            {image === undefined && (
-              <BsFillImageFill
-                className="inputImage"
-                onClick={() => imgRef.current?.click()}
-              />
+
+            {pathname === "/register" && (
+              <>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  className="input"
+                  required
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  name="first_name"
+                  className="input"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  name="last_name"
+                  className="input"
+                  onChange={handleChange}
+                />
+                <textarea
+                  placeholder="Bio ..."
+                  name="bio"
+                  rows={5}
+                  className="input resize-none"
+                  onChange={handleChange}
+                />
+
+                <input
+                  type="file"
+                  className="hidden"
+                  name="image"
+                  required
+                  onChange={fileSelected}
+                  ref={imgRef}
+                />
+
+                {image === undefined && (
+                  <BsFillImageFill
+                    className="inputImage"
+                    onClick={() => imgRef.current?.click()}
+                  />
+                )}
+
+                {prevImage && (
+                  <div className="w-full max-w-[80px] h-[80px] self-end mr-5 top-8 absolute">
+                    <Image
+                      src={prevImage}
+                      alt="prevImage"
+                      fill
+                      loading="lazy"
+                      placeholder="blur"
+                      blurDataURL="/blur.svg"
+                      sizes="(max-width:80px) 100vw"
+                      className="rounded-full object-cover cursor-pointer"
+                      onClick={() => imgRef.current?.click()}
+                    />
+                  </div>
+                )}
+              </>
             )}
 
-            {prevImage && (
-              <div className="w-full max-w-[80px] h-[80px] self-end mr-5 top-8 absolute">
-                <Image
-                  src={prevImage}
-                  alt="prevImage"
-                  fill
-                  placeholder="blur"
-                  sizes='(max-width:80px) 100vw'
-                  loading="lazy"
-                  blurDataURL="/blur.svg"
-                  className="rounded-full object-cover cursor-pointer"
-                />
-              </div>
+            {pathname === "/login" ? (
+              <Link
+                href={"/register"}
+                className="text-[13px] font-semibold text-blue-600 self-end"
+              >
+                No account? Register here
+              </Link>
+            ) : (
+              <Link
+                href={"/login"}
+                className="text-[13px] font-semibold text-blue-600 self-end"
+              >
+                Already have an account? Login here
+              </Link>
             )}
-          </>
-        )}
-        {pathname === "/login" ? (
-          <Link
-            href={"/register"}
-            className="text-[13px] font-semibold text-blue-600 self-end"
-          >
-            No account? Register here
-          </Link>
-        ) : (
-          <Link
-            className="text-[13px] font-semibold text-blue-600 self-end"
-            href={"/login"}
-          >
-            Already have an account? Login here
-          </Link>
-        )}
-        <button className="p-4 rounded-lg bg-blue-600"> {process}</button>
-      </form>
-      <Toaster />
-    </div>
-    ) : (
-<>
-</>
-    )
-    
-   }
-   </>
+
+            <button className="formButton">{process}</button>
+          </form>
+          <Toaster />
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 };
